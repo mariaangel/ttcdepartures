4e28a6813b8bb6415941e0ab31e1938c4e7ac66a
mariaangel <mmarquez.angel@gmail.com>
2014-07-30 13:36:33 -0400

    cleaning

--- ttc.py
+++ ttc.py
@@ -1,14 +1,14 @@
 # ************************************************
-#  
-# ** NAME - TTC departure times 
-# ** 
+#
+# ** NAME - TTC departure times
+# **
 # ** author   M. Angel Marquez-Andrade
 # ** date     30 July 2014
 # ** purpose  Retrieving live data on the
-#             current position of TTC vehicles 
-#             closest to the current user to 
+#             current position of TTC vehicles
+#             closest to the current user to
 #             return approximate departure times
-#             at a specific stop, based on data 
+#             at a specific stop, based on data
 #             from myttc.ca and Nextbus.com
 #
 # ************************************************
@@ -18,7 +18,7 @@
 
 from flask import Flask, render_template, request, jsonify
 from urllib.request import urlopen
-from json import loads, load
+from json import loads
 from xml.dom.minidom import parse
 
 app = Flask(__name__)
@@ -30,13 +30,14 @@ def welcome():
 #return the TTC stops closest to the user
 @app.route('/getStops')
 def getStops():
+
     if 'lat' in request.args and 'lon' in request.args:
         apiUrl = 'http://myttc.ca/near/'
         apiParam = request.args['lat'] + ',' + request.args['lon']
         outputFormat = '.json'
         response = urlopen(apiUrl + apiParam + outputFormat)
         decoded = decode(response)
-        #should check the length of the response
+
         if decoded['locations']:
             decoded = decoded['locations'][0]['stops']
             dicJson = {}
@@ -48,52 +49,49 @@ def getStops():
     else:
         return jsonify( { "error": "Cannot get stops from myttc.ca" } )
 
+#return departure times at the stop
 @app.route('/getDeparture', methods=['POST'])
 def getDeparture():
     if request.method == 'POST':
         s = request.form['stop']
-        if s:
-            #s = s.replace('_',' ')
-            lat = float(request.form[s])
+        lat = float(request.form[s])
 
-            # Get buses that stop at this location (to associate myttc.ca and Nextbus.com)
-            apiUrl = 'http://myttc.ca/'
-            apiParam = s
-            outputFormat = '.json'
-            response = urlopen(apiUrl + apiParam + outputFormat)
-            decoded = decode(response)
-            decoded = decoded['stops'][0]['routes'][0]['uri']
-            vehicle = decoded.split('_')
+        # Get buses that stop at this location (to associate myttc.ca and Nextbus.com)
+        apiUrl = 'http://myttc.ca/'
+        apiParam = s
+        outputFormat = '.json'
+        response = urlopen(apiUrl + apiParam + outputFormat)
+        decoded = decode(response)
+        decoded = decoded['stops'][0]['routes'][0]['uri']
+        vehicle = decoded.split('_')
 
-            # Get id of the stop according to Nextbus.com
-            apiUrl = 'http://webservices.nextbus.com/service/publicXMLFeed?'
-            apiParam = 'command=routeConfig&a=ttc&r=' + vehicle[0]
-            response = urlopen(apiUrl + apiParam)
-            xmldoc = parse(response)
-            itemlist = xmldoc.getElementsByTagName('stop')
-            minStop = itemlist[0].getAttribute('stopId')
-            minSub = abs(lat - float(itemlist[0].getAttribute('lat')))
-            for stop in itemlist:
-                if stop.getAttribute('lat') and abs(lat - float(stop.getAttribute('lat'))) < minSub:
-                    minStop = stop.getAttribute('stopId')
-                    minSub = abs(lat - float(stop.getAttribute('lat')))
+        # Get id of the stop according to Nextbus.com
+        apiUrl = 'http://webservices.nextbus.com/service/publicXMLFeed?'
+        apiParam = 'command=routeConfig&a=ttc&r=' + vehicle[0]
+        response = urlopen(apiUrl + apiParam)
+        xmldoc = parse(response)
+        itemlist = xmldoc.getElementsByTagName('stop')
+        minStop = itemlist[0].getAttribute('stopId')
+        minSub = abs(lat - float(itemlist[0].getAttribute('lat')))
+        for stop in itemlist:
+            if stop.getAttribute('lat') and abs(lat - float(stop.getAttribute('lat'))) < minSub:
+                minStop = stop.getAttribute('stopId')
+                minSub = abs(lat - float(stop.getAttribute('lat')))
 
-            #Get list of departures at that stop
-            apiParam = 'command=predictions&a=ttc&stopId=' + minStop
-            response = urlopen(apiUrl + apiParam)
-            xmldoc = parse(response)
-            itemlist = xmldoc.getElementsByTagName('direction')
-            departures = {}
-            for bus in itemlist:
-                title = bus.getAttribute('title')
-                departures[title] = []
-                predictions = bus.getElementsByTagName('prediction')
-                for p in predictions:
-                    departures[title].append(p.getAttribute('minutes'))
+        #Get list of departures at that stop
+        apiParam = 'command=predictions&a=ttc&stopId=' + minStop
+        response = urlopen(apiUrl + apiParam)
+        xmldoc = parse(response)
+        itemlist = xmldoc.getElementsByTagName('direction')
+        departures = {}
+        for bus in itemlist:
+            title = bus.getAttribute('title')
+            departures[title] = []
+            predictions = bus.getElementsByTagName('prediction')
+            for p in predictions:
+                departures[title].append(p.getAttribute('minutes'))
 
-            return render_template( 'departure.html', d=departures )
-        else:
-            return "Missing required values"
+        return render_template( 'departure.html', d=departures )
 
 def decode(r):
     charset = r.info().get_param('charset', 'utf8')
